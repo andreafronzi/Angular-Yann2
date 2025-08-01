e1f1e990f04c5c27bda45a920680ed8b21310410
AndreaFronzi <andrifronzi@gmail.com>
2025-08-01 16:11:02 +0200

    Update `button-prova` logic and integrate new button with click event
    - Modified `button-prova.component.html` to toggle button's disabled state.
    - Added a primary raised button with click event in `app.component.html`.
    - Imported `ImmaginePropertyBindingComponent` into `app.component.ts`.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,28 @@
 import { Component,ChangeDetectionStrategy } from '@angular/core';
-import {ProvaComponent} from './prova/prova.component';
-import {ButtonProvaComponent} from './button-prova/button-prova.component';
 import {MatProgressBarModule} from '@angular/material/progress-bar';
 import {MatCardModule} from '@angular/material/card';
 import {MatChipsModule} from '@angular/material/chips';
 import {MatIconModule} from '@angular/material/icon';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatButtonModule} from '@angular/material/button';
+import {ImmaginePropertyBindingComponent} from './immagine-property-binding/immagine-property-binding.component';
+import {ButtonProvaComponent} from './button-prova/button-prova.component';
 
 
 @Component({
   selector: 'app-root',
   imports: [
-    ProvaComponent, ButtonProvaComponent,MatProgressBarModule,MatChipsModule,
-    MatCardModule,MatIconModule,MatDividerModule,MatButtonModule
+    MatProgressBarModule, MatChipsModule,
+    MatCardModule, MatIconModule, MatDividerModule, MatButtonModule, ImmaginePropertyBindingComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'corso-angular';
+
+  onclick() {
+    console.log("click");
+  }
 }
 
